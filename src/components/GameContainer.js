cfe443c5079ac0157b3cc8e7211b7d8359fd9748
nagyrobert92 <nagyrobert1992@yahoo.com>
2019-06-07 11:00:29 +0200

    small fix

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -1,40 +1,37 @@
-
-import React from 'react';
-import Game from './Game'
-import { connect } from 'react-redux'
-import { createBoard, checkBoard } from '../actions'
+import React from "react";
+import Game from "./Game";
+import { connect } from "react-redux";
+import { createBoard, checkBoard } from "../actions";
 import swal from "sweetalert";
-
 
 class GameContainer extends React.Component {
   state = {
     board: [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  }
+  };
 
   generateBoard = () => {
-    const newBoard = this.state.board
-    const random = Math.floor(Math.random() * 9)
-    newBoard[random] = 1
-    return newBoard
-  }
-  
-  componentWillMount = () => {
-    this.props.checkBoard()
-    
-    
-    const board = this.generateBoard()
-    this.props.createBoard(board)
-    console.log('Board created')
-  }
+    const newBoard = this.state.board;
+    const random = Math.floor(Math.random() * 9);
+    newBoard[random] = 1;
+    return newBoard;
+  };
 
-  onClick = (event) => {
-    const values = Object.values(this.props.board)
-    const treasure = values.indexOf(1)
-    const button = parseInt(event.target.value)
+  componentWillMount = () => {
+    this.props.checkBoard();
+
+    const board = this.generateBoard();
+    this.props.createBoard(board);
+    console.log("Board created");
+  };
+
+  onClick = event => {
+    const values = Object.values(this.props.board);
+    const treasure = values.indexOf(1);
+    const button = parseInt(event.target.value);
 
     if (button === treasure) {
       return swal({
-        text: "CORRECT!",
+        text: "TREASURE FOUND!",
         buttons: "OK",
         icon: "success"
       });
@@ -61,7 +58,7 @@ function mapStateToProps(state) {
 const mapDispatchToProps = {
   createBoard,
   checkBoard
-}
+};
 
 export default connect(
   mapStateToProps,
